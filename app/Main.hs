51992b2961ba2d38668c7da10eb005c277197fc5
skystar-p <skystar@skystar.dev>
2020-03-21 21:41:21 +0900

    handle null keyword

--- app/Main.hs
+++ app/Main.hs
@@ -2,11 +2,14 @@
 module Main where
 
 import qualified Data.ByteString.Char8 as B
+import Control.Monad (when)
 import System.Environment
+import System.Exit (die)
 import DaumDic
 
 main :: IO ()
 main = do
     words <- getArgs
+    when (null words) $ die "usage: d <word>"
     content <- getContent $ unwords words
     B.putStrLn $ B.intercalate " / " $ parse content
